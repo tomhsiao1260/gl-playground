532eb3930226330af58cde6213248b8caf2457cc
Hsiao Yao <tomtogo1260@gmail.com>
2021-05-18 14:50:21 +0800

    :fire: add jellyfish example
    add jellyfish example

---
+++ dist/examples/jellyfish/fragment.js
@@ -0,0 +1,14 @@
+export default `
+
+uniform float uTime;        // normalized progress time 
+uniform sampler2D uTexture; // texture for particles
+varying vec2 vUv;           // UV coordinate
+varying float vBright;      // brightness
+
+void main() {
+    vec3 color = vec3(vUv, 1.0) * vBright;
+
+    gl_FragColor  = texture2D(uTexture, gl_PointCoord);
+    gl_FragColor *= vec4(color, 1.0);
+}
+`
